3d89cd1c0445ae85bbd78bc67bd3d20df569d24f
ckclr <cookcooller@163.com>
2023-11-30 00:27:08 +0800

    new config

---
+++ custom.el
@@ -0,0 +1,28 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(embark-consult embarkconsult zenburn-theme which-key vertico rg restart-emacs projectile plantuml-mode pdf-tools ox-hugo org-transclusion org-roam org-remark org-noter org-journal org-fragtog org-bullets orderless marginalia expand-region embark dired-sidebar consult company blacken benchmark-init beacon async ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-block ((t (:inherit :default))))
+ '(org-block-begin-line ((t (:inherit :default))))
+ '(org-block-end-line ((t (:inherit :default))))
+ '(org-code ((t (:inherit :default))))
+ '(org-document-info ((t (:inherit :default))))
+ '(org-document-info-keyword ((t (:inherit :default))))
+ '(org-document-title ((t (:inherit :default))))
+ '(org-drawer ((t (:inherit :default))))
+ '(org-level-1 ((t (:inherit :default :weight bold :height 1.0))))
+ '(org-level-2 ((t (:inherit :default :weight bold :height 1.1))))
+ '(org-level-3 ((t (:inherit :default :weight bold :height 1.2))))
+ '(org-level-4 ((t (:inherit :default :weight bold :height 1.3))))
+ '(org-meta-line ((t (:inherit :default))))
+ '(org-property-value ((t (:inherit :default))) t)
+ '(org-special-keyword ((t (:inherit :default))))
+ '(org-table ((t (:inherit :default)))))
